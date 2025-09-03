a0a09bd9bfc2cd4376986bc7560b2c4ed857c33d
Seif Talaat <Seifmtalaat@gmail.com>
2025-09-03 18:50:40 +0300

    Add Xcode project for Present

---
+++ Presentste s/Presentste s/Presentste_sApp.swift
@@ -0,0 +1,17 @@
+//
+//  Presentste_sApp.swift
+//  Presentste s
+//
+//  Created by Coding on 03/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct Presentste_sApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Presentste s/Presentste sUITests/Presentste_sUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Presentste_sUITestsLaunchTests.swift
+//  Presentste sUITests
+//
+//  Created by Coding on 03/09/2025.
+//
+
+import XCTest
+
+final class Presentste_sUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
